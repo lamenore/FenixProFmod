8d506163213d9c6d5dff7f24e477ff96a1917514
LAMENORE <danilosanchesferme@gmail.com>
2024-11-11 20:03:21 -0300

    Initial commit, Removed unused files, added rivals 2 keys on input and output

---
+++ FenixProFmodAva/ViewModels/FsbKeys.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FenixProFmodAva.ViewModels
+{
+    internal class FsbKeys
+    { 
+
+
+        public static string[] fsbkey_list = [
+            "resoforce" // Rivals of Aether 2 (PC), release
+        ];
+
+    }
+}
